da659373be3a4614a5b869531b377a7bc714cae4
John Paredes <john.paredes@gmail.com>
2013-09-24 22:04:39 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cf7bHomework::Application.config.secret_key_base = '3af7ba596e2ada401233d12502a32f12c019f8a6b1a067d06c88b22299ae5904b2d3bd0962cb25ff520fc4f93b78a2eb443d7d80f15599e9bec87be00e871449'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cf7bHomework::Application.config.session_store :cookie_store, key: '_cf_7b_homework_session'
